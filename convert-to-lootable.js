7b1dc7e3203c1485a913997e37fa1db13f2bdb2a
unsoluble <unsoluble@me.com>
2020-12-29 17:58:15 -0800

    Create convert-to-lootable.js

---
+++ convert-to-lootable.js
@@ -0,0 +1,31 @@
+let d = new Dialog({
+title: 'Convert to lootable body',
+content: `Sure?`,
+buttons: {
+    no: {
+        icon: '<i class="fas fa-ban"></i>',
+        label: 'Cancel'
+    },
+    yes: {
+        icon: '<i class="fas fa-thumbs-up"></i>',
+        label: 'Convert',
+        callback: (html) => {
+            main();
+        }
+    },
+},
+default: 'no',
+}).render(true);
+
+async function main() {
+    for (let token of canvas.tokens.controlled) {
+        token.actor.setFlag("core", "sheetClass", "dnd5e.LootSheet5eNPC");
+        token.update({
+            'actorData.permission.default': ENTITY_PERMISSIONS.OBSERVER,
+            overlayEffect : `icons/svg/chest.svg`
+        });
+        const feats = token.actor.items.filter(i => i.type === "feat");
+        const deletions = feats.map(i => i._id);
+        token.actor.deleteEmbeddedEntity("OwnedItem", deletions);
+    }
+}
