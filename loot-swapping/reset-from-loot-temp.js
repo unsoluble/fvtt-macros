80cff865dcc82f4dde884b5230bc74dc92e41f1b
John Weston <unsoluble@me.com>
2021-01-05 09:11:02 -0800

    Add awaits & remove overlay

--- loot-swapping/reset-from-loot-temp.js
+++ loot-swapping/reset-from-loot-temp.js
@@ -2,9 +2,10 @@
 
 async function main() {
     for (let token of canvas.tokens.controlled) {
-        token.actor.setFlag("core", "sheetClass", "Default");
-        token.update({
-            'actorData.permission.default': ENTITY_PERMISSIONS.NONE
+        await token.actor.setFlag("core", "sheetClass", "Default");
+        await token.update({
+            'actorData.permission.default': ENTITY_PERMISSIONS.NONE,
+            overlayEffect : ''
         });
     }
 }
