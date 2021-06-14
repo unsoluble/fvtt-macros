7d368a70925e65e54864c20625f96763f3b905c9
unsoluble <unsoluble@me.com>
2021-06-14 03:24:53 +0000

    Cleanup

--- inspiration-cards.js
+++ inspiration-cards.js
@@ -1,6 +1,6 @@
-const table = game.tables.getName("Inspiration Deck");
-const packName = "SharedCompendiums.inspiration-cards";
-const actors = ["Barnabus Truesheel", "Lachish", "Natriel Adonai", "Owlgar", "Phingo Underfoot"];
+const table = game.tables.getName('Inspiration Deck');
+const packName = 'SharedCompendiums.inspiration-cards';
+const actors = ['Barnabus Truesheel', 'Lachish', 'Natriel Adonai', 'Owlgar', 'Phingo Underfoot'];
 
 const DistributeItems = false;
 const DeleteExisting = false;
@@ -21,118 +21,118 @@ let messageBody = `<div style='
 let rolls = [];
 
 (async () => {
-    for (const actor of actors) {
-      await doDraw(actor);
-      if (DeleteExisting === true) {
-        let existingCards = game.actors.getName(actor).items
-          .filter(item => {
-            if (item.data.data.source == 'Inspiration Deck') {
-              return true;
-            }
-          });
-        for (let i of existingCards) { 
-          await game.actors.getName(actor).deleteOwnedItem(i.id)
-          console.log("Deleted " + i.name + " from " + actor);
+  for (const actor of actors) {
+    // Draw a card for each actor.
+    await doDraw(actor);
+    // If desired, delete any existing inspiration cards from the actors.
+    if (DeleteExisting === true) {
+      let existingCards = game.actors.getName(actor).items.filter((item) => {
+        if (item.data.data.source == 'Inspiration Deck') {
+          return true;
         }
+      });
+      for (let i of existingCards) {
+        await game.actors.getName(actor).deleteOwnedItem(i.id);
+        console.log('Deleted ' + i.name + ' from ' + actor);
       }
     }
-    
-    // Close the BG div at the end.
-    messageBody += "</div>";
-    
-    let chatData = {
-        user: game.user._id,
-        speaker: ChatMessage.getSpeaker({alias: "Inspiration!"}),
-        content: messageBody
-    }
-    ChatMessage.create(chatData, {});
+  }
+
+  // Close the BG div at the end.
+  messageBody += '</div>';
+
+  let chatData = {
+    user: game.user._id,
+    speaker: ChatMessage.getSpeaker({ alias: 'Inspiration!' }),
+    content: messageBody,
+  };
+  ChatMessage.create(chatData, {});
 })();
 
 async function doDraw(actor) {
-    let roll = '';
-    do {
-        roll = new Roll(table.data.formula).roll();
-    } while (rolls.includes(roll.total));
-    
-    rolls.push(roll.total);
-    const result = table._getResultsForRoll(roll.total)[0];
-    
-    const destinationActor = game.actors.getName(actor);
-    
-    let item = await searchItem({ key : packName, name : result.text });
-    let entity = await findItemInCompendium({ keys : packName, name : item.name, id: item._id })
-    
-    messageBody
-        += `<div style='
-            color: #efefe0;
-            margin-top: 5px;
-            text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.8);
-            '>`
-        + actor
-        + ` gets</div><a
-                class='entity-link'
-                data-id='` + item._id + `'
-                data-pack='SharedCompendiums.inspiration-cards'
-                style='
-                border: none;
-                background: none;'>
-                <img style='
-                width: 80%;
-                margin-top: 2px;
-                border: none;
-            'src=`
-        + buildImageURL(item.name)
-        + "></a><br>";
-    
-    if (DistributeItems) await destinationActor.createOwnedItem(entity);
+  let roll = '';
+  do {
+    roll = new Roll(table.data.formula).roll();
+  } while (rolls.includes(roll.total));
+
+  rolls.push(roll.total);
+  const result = table._getResultsForRoll(roll.total)[0];
+
+  const destinationActor = game.actors.getName(actor);
+
+  let item = await searchItem({ key: packName, name: result.text });
+  let entity = await findItemInCompendium({ keys: packName, name: item.name, id: item._id });
+
+  messageBody +=
+    `<div style='
+      color: #efefe0;
+      margin-top: 5px;
+      text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.8);
+      '>` +
+    actor +
+    ` gets</div><a
+      class='entity-link'
+      data-id='` +
+    item._id +
+    `'data-pack='SharedCompendiums.inspiration-cards'
+      style='
+      border: none;
+      background: none;'>
+      <img style='
+      width: 80%;
+      margin-top: 2px;
+      border: none;
+    'src=` +
+    buildImageURL(item.name) +
+    '></a><br>';
+
+  if (DistributeItems) await destinationActor.createOwnedItem(entity);
 }
 
 // These search and find functions I'm sure could be consolidated/simplified,
 // but they work as-is so I ain't touching them.
-async function searchItem({ key , name = "", }){
-    let pack = game.packs.get(key);
-    let pack_index = pack.index.length > 1 ? pack.index : (await pack.getIndex());
-    let item_index = pack_index.find(i=>i.name.toLowerCase() === name.toLowerCase());
-    if(item_index)
-      return {
-        comp_link : `@Compendium[${key}.${item_index._id}]{${item_index.name}}`,
-        img_link : `<img src="${item_index.img}" width="25" height="25">`,
-        key : key,
-        img : item_index.img,
-        name : item_index.name,
-        _id : item_index._id,
-      };
+async function searchItem({ key, name = '' }) {
+  let pack = game.packs.get(key);
+  let pack_index = pack.index.length > 1 ? pack.index : await pack.getIndex();
+  let item_index = pack_index.find((i) => i.name.toLowerCase() === name.toLowerCase());
+  if (item_index)
+    return {
+      comp_link: `@Compendium[${key}.${item_index._id}]{${item_index.name}}`,
+      img_link: `<img src="${item_index.img}" width="25" height="25">`,
+      key: key,
+      img: item_index.img,
+      name: item_index.name,
+      _id: item_index._id,
+    };
   return undefined;
 }
 
-async function findItemInCompendium({ keys = [], name = "", id = ""}) {
-    keys = keys instanceof Array ? keys : [keys];
-    for(let key of keys) {
-        let pack = game.packs.get(key);
-        let itemID = id ?? (await pack.getIndex()).find(i=>i.name === name)?._id;
-        if(itemID) return await pack.getEntity(itemID);
-    }
+async function findItemInCompendium({ keys = [], name = '', id = '' }) {
+  keys = keys instanceof Array ? keys : [keys];
+  for (let key of keys) {
+    let pack = game.packs.get(key);
+    let itemID = id ?? (await pack.getIndex()).find((i) => i.name === name)?._id;
+    if (itemID) return await pack.getEntity(itemID);
+  }
 }
 
 function buildImageURL(name) {
-    let theURL = "/assets/Inspiration/standard/" + string_to_slug(name) + ".webp";
-    return theURL;
+  let theURL = '/assets/Inspiration/standard/' + string_to_slug(name) + '.webp';
+  return theURL;
 }
 
-function string_to_slug (str) {
-    str = str.replace(/^\s+|\s+$/g, ''); // trim
-    str = str.toLowerCase();
-  
-    // remove accents, swap ñ for n, etc
-    var from = "àáäâèéëêìíïîòóöôùúüûñç·/_,:;";
-    var to   = "aaaaeeeeiiiioooouuuunc------";
-    for (var i=0, l=from.length ; i<l ; i++) {
-        str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
-    }
-
-    str = str.replace(/[^a-z0-9 -]/g, '') // remove invalid chars
-        .replace(/\s+/g, '-') // collapse whitespace and replace by -
-        .replace(/-+/g, '-'); // collapse dashes
-
-    return str;
+function string_to_slug(str) {
+  str = str.replace(/^\s+|\s+$/g, ''); // trim
+  str = str.toLowerCase();
+  // remove accents, swap ñ for n, etc
+  var from = 'àáäâèéëêìíïîòóöôùúüûñç·/_,:;';
+  var to = 'aaaaeeeeiiiioooouuuunc------';
+  for (var i = 0, l = from.length; i < l; i++) {
+    str = str.replace(new RegExp(from.charAt(i), 'g'), to.charAt(i));
+  }
+  str = str
+    .replace(/[^a-z0-9 -]/g, '') // remove invalid chars
+    .replace(/\s+/g, '-') // collapse whitespace and replace by -
+    .replace(/-+/g, '-'); // collapse dashes
+  return str;
 }
