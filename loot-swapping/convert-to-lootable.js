a4073a72f40593c16b9c48dc42c28664d3e00cfb
Muuk-e <99773359+Muuk-e@users.noreply.github.com>
2022-02-15 16:38:32 -0800

    Added fix due to lootsheet module change
    Chang line 54 
    'dnd5e.LootSheet5eNPC' to 'dnd5e.LootSheetNPC5e' 
    to match the lootsheet module, so that it actually works

--- loot-swapping/convert-to-lootable.js
+++ loot-swapping/convert-to-lootable.js
@@ -1,7 +1,7 @@
 // For all currently selected tokens, changes their actor sheet to LootSheetNPC,
 // deletes all non-lootable items, sets token Observer privs for players, and adds a treasure
 // overlay icon to the body. Asks for confirmation because of the deletions.
-//
+// updated to v9 by @Muuk-e
 // Original script by tas (ygg#0922) -- thanks tas!
 // Evolved with help from a ton of people, notably: @Akaito, @honeybadger, @kekilla, and @cole
 
@@ -48,10 +48,10 @@ async function ConvertToLootable() {
     await token.document.actor.deleteEmbeddedDocuments('Item', itemsToDelete);
 
     // Change sheet to lootable, and give players permissions.
-    let newActorData = {
+       let newActorData = {
       flags: {
         core: {
-          sheetClass: 'dnd5e.LootSheet5eNPC',
+          sheetClass: 'dnd5e.LootSheetNPC5e',
         },
         lootsheetnpc5e: {
           lootsheettype: 'Loot',
